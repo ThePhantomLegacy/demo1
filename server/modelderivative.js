17e14457f586fb3d1c924a201fa7f63c83fce61c
Pla <EPla@whiting-turner.com>
2018-06-04 15:02:54 -0400

    v0.0.5 Added modelDerivative.js

---
+++ server/modelderivative.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// Web framwork
+var express = require('express');
+var router = express.Router();
+
+// Forge NPM
+var forgeSDK = require('forge-apis');
+
+// Handle json requests
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
+// Actually perform the token operation
+var oauth = require('./oauth');
+
+// Create a new bucket
+router.post('/api/forge/modelderivative/jobs', jsonParser, 
+    function(req, res)
+    {
+        oauth.getTokenInternal().then(
+            function(credentials)
+            {
+                // Prepare the translation job payload
+                var postJob = new forgeSDK.JobPayload();
+                postJob.input = new forgeSDK.JobPayloadInput();
+                postJob.input.urn = req.body.objectName;
+                postJob.output = new forgeSDK.JobPayloadOutput(
+                    [new forgeSDK.JobSvfOutputPayload()]
+                );
+                postJob.output.formats[0].type = 'svf';
+                postJob.output.formats[0].views = ['2d','3d'];
+
+                // Create the derivative API
+                var derivativesApi = new forgeSDK.derivativesAPI();
+                // Post the job
+                derivativesApi.translate(postJob, {}, oauth.OAuthClient(), credentials)
+                    .then(function(data)
+                    {
+                        res.status(200).end;
+                    }
+                ).catch(function(e)
+                    {
+                        console.log('Error at Model Derivative job:');
+                        console.log(e);
+                        res.status(500).json({ error: e.error.body });
+                    }
+                );
+            }
+        );
+    }
+);
+
+module.exports = router;
